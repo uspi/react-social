e1127f071ac778d28e97ba31fe564837c21d05d0
uspi <snowdogloose@gmail.com>
2021-07-03 12:38:46 +0300

    added style

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,7 +3,7 @@ import s from "./ProfileInfo.module.css";
 
 const ProfileInfo = () => {
     return (
-        <div>
+        <div className={s.profileInfo}>
 
             <div className={s.cover_img}>
                 <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" />
