1666febf478ffb285097f3be4dd0a9a0dacfd6ca
uspi <snowdogloose@gmail.com>
2021-07-04 23:13:40 +0300

    file "state.js" renamed to "store.js"

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import store from "./redux/state";
+import store from "./redux/store";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
