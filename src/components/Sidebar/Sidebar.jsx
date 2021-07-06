0ba5bd1d603e9ff57754e71e9d318d1491400db3
uspi <snowdogloose@gmail.com>
2021-07-06 22:53:35 +0300

    deleted store context

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,32 +1,17 @@
 import React from "react";
-import StoreContext from "../../StoreContext";
 import style from "./Sidebar.module.css";
 import SidebarItem from "./SidebarItem/SidebarItem";
 
 const Sidebar = (props) => {
-    // let state = props.store.getState();
 
-    // let sidebarElements = state.sidebar.friends.map(e => <SidebarItem id={e.id} name={e.name} />);
     let sidebarElements = props.friends.map(e =>
         <SidebarItem key={e.id} id={e.id} name={e.name} />
     );
     return (
-        <StoreContext.Consumer>{
-            (store) => {
-                let state = store.getState();
 
-                let sidebarElements =
-                    state.sidebar.friends.map(e =>
-                        <SidebarItem id={e.id} name={e.name} />
-                    );
-                return (
-                    <div className={style.sidebar}>
-                        {sidebarElements}
-                    </div>
-                );
-            }
-        }
-        </StoreContext.Consumer>
+        <div className={style.sidebar}>
+            {sidebarElements}
+        </div>
     );
 }
 
