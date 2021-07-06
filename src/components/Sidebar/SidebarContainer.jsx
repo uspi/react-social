27a70d82d73bf54ff66abb31470eb7329a68dea0
uspi <snowdogloose@gmail.com>
2021-07-06 22:41:41 +0300

    container of sidebar

---
+++ src/components/Sidebar/SidebarContainer.jsx
@@ -0,0 +1,17 @@
+import { connect } from "react-redux";
+import Sidebar from "./Sidebar";
+
+const mapStateToProps = (state) => {
+    return {
+        friends: state.sidebar.friends
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {}
+}
+
+const SidebarContainer =
+    connect(mapStateToProps, mapDispatchToProps)(Sidebar);
+
+export default SidebarContainer;
