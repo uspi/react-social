aec97d890e11d80b62915399498316b21fdf8d8c
uspi <snowdogloose@gmail.com>
2021-07-17 20:40:07 +0300

    auth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import style from "./Header.module.css";
 import tLogo from "./../../assets/images/tLogo512.png";
+import { NavLink } from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <div className={style.headerContainer}>
             <header className={style.header}>
                 <div className={style.logo}>
                     <img src={tLogo} alt="logo of company"/>
                 </div>
-            </header>
+                <div className={style.loginBlock}>
+                    {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink>}
+                    
+                </div>
+            </header>       
         </div>
     );
 }
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,6 +3,7 @@ import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import { combineReducers, createStore } from "redux";
 import usersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
 
 
 let reducers = combineReducers({
@@ -10,6 +11,7 @@ let reducers = combineReducers({
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
     sidebar: sidebarReducer,
+    auth: authReducer,
 });
 
 let store = createStore(reducers);
