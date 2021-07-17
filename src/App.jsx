43af9754abab83766c9231d5c367cc16fa46f444
uspi <snowdogloose@gmail.com>
2021-07-17 20:40:34 +0300

    added header container

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import Header from "./components/Header/Header";
+import HeaderContainer from "./components/Header/HeaderContainer";
 import Music from "./components/Music/Music";
 import Navbar from "./components/Navbar/Navbar";
 import News from "./components/News/News";
@@ -15,7 +15,7 @@ const App = (props) => {
   return (
     <div className="app-wrapper">
       <div className="header">
-        <Header />
+        <HeaderContainer />
       </div>
 
       <div className="app-wrapper-content">
