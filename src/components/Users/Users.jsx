8d659b6589921aad184c16c49aa236e4e2c3d1aa
uspi <snowdogloose@gmail.com>
2021-07-17 20:41:35 +0300

    disable current btn if following progress

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import style from "./Users.module.css"
 import userPhoto from "./../../assets/images/user-no-photo.png"
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 let Users = (props) => {
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -11,22 +12,21 @@ let Users = (props) => {
         pages = [...pages, i];
     }
 
-    let quantityPageButtons = 24;
+    // let quantityPageButtons = 24;
     let filteredPages = [];
     for (let i = 0; i < pages.length; i++) {
 
-        if(i > props.currentPage - 5 && i< props.currentPage + 6){
-            filteredPages = [...filteredPages, i+1]
+        if (i > props.currentPage - 5 && i < props.currentPage + 6) {
+            filteredPages = [...filteredPages, i + 1]
             continue;
         }
     }
-
     return (
         <div className={style.usersContainer}>
             <div className={style.paginationButtonsContainer}>
                 {
                     filteredPages.map(p => (
-                        <div
+                        <div key={p}
                             className={props.currentPage === p ?
                                 style.selectedPageNumberBtn
                                 : style.pageNumberBtn}
@@ -43,7 +43,9 @@ let Users = (props) => {
                     <div key={u.id} className={style.item}>
                         <NavLink to={"/profile/" + u.id}>
                             <div className={style.userPhoto}>
-                                <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="user avatar" />
+                                <img src={u.photos.small != null ?
+                                    u.photos.small
+                                    : userPhoto} alt="user avatar" />
                             </div>
                         </NavLink>
                         <div className={style.userDescription}>
@@ -57,10 +59,44 @@ let Users = (props) => {
                         </div>
                         <div className={style.buttonContainer}>
                             {u.followed ?
-                                <button className={style.button}
-                                    onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                                : <button className={style.button}
-                                    onClick={() => { props.follow(u.id) }}>Follow</button>
+                                <button disabled={props.followingInProgress.some(id => id === u.id)}
+                                    className={style.button}
+                                    onClick={() => {
+                                        props.toggleFollowingProgress(true, u.id);
+                                        axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                            withCredentials: true,
+                                            headers: {
+                                                "API-KEY": "d208ae23-b630-4cf0-abb9-16089b6de9c2"
+                                            }
+                                        })
+                                            .then(response => {
+                                                if (response.data.resultCode === 0) {
+                                                    props.unfollow(u.id)
+                                                }
+                                                props.toggleFollowingProgress(false, u.id);
+                                            });
+
+
+
+                                    }}>Unfollow</button>
+                                : <button disabled={props.followingInProgress.some(id => id === u.id)} className={style.button}
+                                    onClick={() => {
+                                        props.toggleFollowingProgress(true, u.id);
+                                        axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                            withCredentials: true,
+                                            headers: {
+                                                "API-KEY": "d208ae23-b630-4cf0-abb9-16089b6de9c2"
+                                            }
+                                        })
+                                            .then(response => {
+                                                if (response.data.resultCode === 0) {
+                                                    props.follow(u.id)
+                                                }
+                                                props.toggleFollowingProgress(false, u.id);
+                                            });
+
+
+                                    }}>Follow</button>
                             }
                         </div>
                     </div>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -4,6 +4,7 @@ const SET_USERS = "SET-USERS";
 const SET_CURRENT_PAGE = "SET-CURRENT-PAGE";
 const SET_TOTAL_USERS_COUNT = "SET-TOTAL-USERS-COUNT";
 const TOGGLE_IS_FETCHING = "TOGGLE-IS-FETCHING";
+const FOLLOWING_IN_PROGRESS = "FOLLOWING-IN-PROGRESS";
 
 let initialState = {
   users: [],
@@ -11,6 +12,7 @@ let initialState = {
   totalUsersCount: 0,
   currentPage: 1,
   isFetching: true,
+  followingInProgress: [],
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -62,6 +64,13 @@ const usersReducer = (state = initialState, action) => {
         ...state,
         isFetching: action.isFetching,
       };
+    case FOLLOWING_IN_PROGRESS:
+      return {
+        ...state,
+        followingInProgress: action.isFetching 
+        ? [...state.followingInProgress, action.userId]
+        : state.followingInProgress.filter(id => id !== action.userId),
+      };
     default:
       return state;
   }
@@ -81,6 +90,11 @@ export const setTotalUsersCount = (totalUsersCount) => ({
 export const toggleIsFetching = (isFetching) => ({
   type: TOGGLE_IS_FETCHING,
   isFetching,
+});
+export const toggleFollowingProgress = (isFetching, userId) => ({
+  type: FOLLOWING_IN_PROGRESS,
+  isFetching,
+  userId,
 });
 
 export default usersReducer;
