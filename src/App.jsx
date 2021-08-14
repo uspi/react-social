18cb34d198a77151aa20b2e4750a9be6d296a11d
uspi <snowdogloose@gmail.com>
2021-08-14 10:26:21 +0300

    switch for routes, not found page handling, promise rejection catch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, withRouter } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch, withRouter } from "react-router-dom";
 import "./App.css";
 import { connect, Provider } from "react-redux";
 import { initializeApp } from "./redux/app-reducer"
@@ -16,14 +16,24 @@ import SidebarContainer from "./components/Sidebar/SidebarContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import Preloader from "./components/common/Preloader/Preloader";
 import HeaderContainer from "./components/Header/HeaderContainer";
-import { withSuspense } from "./hoc/withSuspense";
+import { HttpError } from "./components/common/NotFound/HttpError";
+//import { withSuspense } from "./hoc/withSuspense";
 
 const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"));
 const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"));
 
 class App extends React.Component {
+  catchAllUnhandledPromiseRejection = (reason, promiseRejectionEvent) => {
+    alert("Some error occurred");
+    console.error(promiseRejectionEvent);
+  }
+
   componentDidMount() {
     this.props.initializeApp();
+    window.addEventListener("unhandledrejection", this.catchAllUnhandledPromiseRejection);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("unhandledrejection", this.catchAllUnhandledPromiseRejection);
   }
 
   render() {
@@ -41,35 +51,49 @@ class App extends React.Component {
         <div className="app-wrapper-content">
           <Navbar />
           <SidebarContainer store={this.props.store} />
-
-          <Route
-            path="/profile/:userId?"
-            render={() => (
-              <React.Suspense fallback={<div className="content-area">Loading...</div>}>
-                <ProfileContainer />
-              </React.Suspense>
-            )}
-          />
-          <Route
-            path="/dialogs"
-            render={() => (
-              <React.Suspense fallback={<div className="content-area">Loading...</div>}>
-                <DialogsContainer />
-              </React.Suspense>
-            )}
-          />
-          <Route path="/news" render={() => <News />} />
-          <Route path="/music" render={() => <Music />} />
-          <Route path="/users" render={() => <UsersContainer />} />
-          <Route path="/settings" render={() => <Settings />} />
-
-          <Route path="/login" render={() => <Login />} />
+          <Switch>
+            <Route
+              path="/profile/:userId?"
+              render={() => (
+                <React.Suspense fallback={<SuspenseLoading />}>
+                  <ProfileContainer />
+                </React.Suspense>
+              )}
+            />
+            <Route
+              path="/dialogs"
+              render={() => (
+                <React.Suspense fallback={<SuspenseLoading />}>
+                  <DialogsContainer />
+                </React.Suspense>
+              )}
+            />
+            <Route path="/news" render={() => <News />} />
+            <Route path="/music" render={() => <Music />} />
+            <Route path="/users" render={() => <UsersContainer />} />
+            <Route path="/settings" render={() => <Settings />} />
+            <Route path="/login" render={() => <Login />} />
+            <Route
+              exact path="/"
+              render={() => <Redirect to="/profile" />}
+            />
+            
+            <Route path="*" render={() =>
+              <HttpError httpCode="404" httpText="Page Not Found"
+                errorHeader="No such page was found."
+                errorText="If you went to this page by accident, go back to the previous page."
+              />} />
+          </Switch>
         </div>
       </div>
 
     );
   }
 };
+
+const SuspenseLoading = () => {
+  return <div className="content-area">Loading...</div>
+}
 
 const mapStateToProps = (state) => ({
   initialized: state.app.initialized,
@@ -86,7 +110,7 @@ let AppContainer = compose(
 // browser-router => store-provider => container
 let MainApp = (props) => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <AppContainer />
       </Provider>
