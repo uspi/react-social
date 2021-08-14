110aba0fde72dec46212d67fe6c0d22831cd99c3
uspi <snowdogloose@gmail.com>
2021-08-14 10:26:56 +0300

    clear unused imports

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import store from "./redux/redux-store";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 import MainApp from "./App";
 
 ReactDOM.render(
