5891369664f8a07bf095dc4528ded6fa1536d4e2
uspi <snowdogloose@gmail.com>
2021-08-14 10:36:48 +0300

    user status tip

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHooks.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react"
+//import style from "./ProfileStatus.module.css"
 
 const ProfileStatusWithHooks = (props) => {
 
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
+    let [tip, setTip] = useState(false);
 
     // after every render, if haven't dependencies
     useEffect(() => {
@@ -11,7 +13,9 @@ const ProfileStatusWithHooks = (props) => {
     }, [props.status]);
 
     const activateEditMode = () => {
+        if (!props.isOwner) return;
         setEditMode(true);
+        setTip(false);
     };
 
     const deactivateEditMode = () => {
@@ -19,13 +23,22 @@ const ProfileStatusWithHooks = (props) => {
         props.updateUserStatus(status);
     };
 
+    const showStatusTip = () => {
+        // TODO: replace by thunk and state prop
+        setTimeout(() => setTip(true), 200)
+    }
+
+    const hideStatusTip = () => {
+        setTip(false);
+    }
+
     const onStatusChange = (e) => {
         setStatus(e.currentTarget.value);
     };
 
     return (
-        <>
-            {!editMode && <div onDoubleClick={activateEditMode}>
+        <div>
+            {!editMode && <div onMouseEnter={showStatusTip} onMouseLeave={hideStatusTip} onDoubleClick={activateEditMode}>
                 {props.status || "_-_-_-_"}
             </div>}
 
@@ -39,7 +52,10 @@ const ProfileStatusWithHooks = (props) => {
                     />
                 </div>
             }
-        </>
+            {/* {tip && props.isOwner &&
+                <div className={style.profileStatusTip}>(?) double click for change</div>
+            } */}
+        </div>
     );
 }
 
